18999605f88542742d5a04517a743bb641d7a6ba
Sisi Zhang <zhangsisi0717@gmail.com>
2021-03-26 12:57:29 -0500

    kadane algorithm

--- DPKConcatenationMaximumSum.py
+++ DPKConcatenationMaximumSum.py
@@ -1,4 +1,5 @@
 class kConcatenationMaxSum:  # https://leetcode.com/problems/k-concatenation-maximum-sum/
+    import numpy as np
     def kConcatenationMaxSum(self, arr, k):
         base = 10 ** 9 + 7
         curMax = 0
@@ -26,8 +27,39 @@ class kConcatenationMaxSum:  # https://leetcode.com/problems/k-concatenation-max
             realIdx += 1
         return globalMax % base
 
+
+    def kadaneAlgorithm(self,array):
+        curMaxSub = [array[0]]
+        curMax = array[0]
+        globalMaxSum = array[0]
+        globalMaxSubArray = [array[0]]
+
+        for i in range(1,len(array)):
+            if curMax>0:
+                curMax += array[i]
+                curMaxSub = curMaxSub + [array[i]]
+            else:
+                curMax = array[i]
+                curMaxSub = [array[i]]
+
+            if curMax>globalMaxSum:
+                globalMaxSum = curMax
+                globalMaxSubArray = curMaxSub
+
+        return globalMaxSum,globalMaxSubArray
+
+
+
+
+
+
+
+
+
+
 """
-Kadane's algorithms: keep track of current_Max_sum and global_Max_Sum, scan array from i=0 to (N-1), 
+Kadane's algorithms: keep track of maximum subarray ending at index i and global_Max_Sum, scan array from i=0 to (N-1),
+So for index i, the max subarray ending at index i == array[i]  or [MaxSubArrayEndingAtIndex i-1 ] + array[i]
 if current_Max_sum (ending at index i) = array[i]:
     current_Max_Sum<=0
 else:
