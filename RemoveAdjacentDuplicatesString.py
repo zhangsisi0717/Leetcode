5e5df5914fb15c4f58a4906f899dbd81722676e5
Sisi Zhang <zhangsisi0717@gmail.com>
2021-05-25 15:44:27 -0500

    removeDup

---
+++ RemoveAdjacentDuplicatesString.py
@@ -0,0 +1,26 @@
+#https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/fu
+def removeDuplicates(self, s: str, k: int) -> str:
+    string = s
+    change = True
+    while(change):
+        new_s = ""
+        change = False
+        prev = string[0]
+        count = 1
+        for i in range(1,len(string)):
+            if string[i] == prev:
+                count += 1
+                if count == k:
+                    count =0
+                    change = True
+                continue
+
+
+            new_s += count*prev
+            prev = string[i]
+            count = 1
+
+        new_s = new_s + count*prev
+        string = new_s
+
+    return new_s
