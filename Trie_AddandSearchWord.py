13eec6d4f2e1e4642157c5f10e9482f0380b2a2f
Sisi Zhang <zhangsisi0717@gmail.com>
2021-05-27 22:33:51 -0500

    Trie application

--- Trie_AddandSearchWord.py
+++ Trie_AddandSearchWord.py
@@ -13,24 +13,24 @@ class WordDictionary:
                 cur_dict["end"] = True
 
     def search(self, word: str) -> bool:
-        def has(w,cur_dict):
-            if not w and "end" in cur_dict:return True
-            if not w and "end" not in cur_dict:return False
-            for i in range(len(w)):
-                if w[i] != "." and w[i] in cur_dict:
-                    cur_dict = cur_dict[w[i]]
+        def has(idx,cur_dict,word):
+            if idx == len(word) and "end" in cur_dict: return True
+            if idx == len(word)  and "end" not in cur_dict: return False
+            for i in range(idx,len(word)):
+                if word[i] != "." and word[i] in cur_dict:
+                    cur_dict = cur_dict[word[i]]
 
-                elif w[i]!= "." and w[i] not in cur_dict:
+                elif word[i]!= "." and word[i] not in cur_dict:
                     return False
 
-                elif w[i] == ".":
+                elif word[i] == ".":
                     for sub_dict in cur_dict.values():
-                        if sub_dict != True and has(w[i+1:],sub_dict):
+                        if sub_dict != True and has(i+1,sub_dict,word):
                             return True
                     return False
 
-                if i == len(w)-1 and "end" not in cur_dict:
+                if i == len(word)-1 and "end" not in cur_dict:
                     return False
             return True
 
-        return has(word,self.wordDict)
+        return has(0,self.wordDict,word)
