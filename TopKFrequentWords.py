aa6be67c1e5fc1e5d10218aad4569ee2d236a018
Sisi Zhang <zhangsisi0717@gmail.com>
2021-06-01 17:16:13 -0500

    TopK

--- TopKFrequentWords.py
+++ TopKFrequentWords.py
@@ -1,7 +1,28 @@
-##https://leetcode.com/problems/top-k-frequent-words/
 from type_checking import *
 from collections import Counter
 import random
+import heapq
+##https://leetcode.com/problems/top-k-frequent-words/
+
+
+"""
+approach 1: heap (nlogk) => if we want to get most frequent k words, we need to keep track of a "min_heap" with size k
+"""
+##https://leetcode.com/problems/top-k-frequent-elements/
+def topKFrequent(nums, k):
+    num_to_count = Counter(nums)
+    num_list = [(j,i) for i,j in num_to_count.items()]
+    min_heap = num_list[:k]
+    heapq.heapify(min_heap)
+    for idx in range(k,len(num_list)):
+        if num_list[idx][0]>min_heap[0][0]:
+            heapq.heapreplace(min_heap,num_list[idx])
+    return [re[1] for re in min_heap]
+
+"""
+approach 2: quick sort(keep track of k smallest elements)
+"""
+
 def topKFrequent(words: List[str], k: int) -> List[str]:
     counter = Counter(words)
     unique = [i for i in counter.keys()]
