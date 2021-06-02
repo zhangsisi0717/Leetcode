74062dede996ef57cd6158799498a0bc67134c58
Sisi Zhang <zhangsisi0717@gmail.com>
2021-06-02 15:01:04 -0500

    CoinChange.py

---
+++ DP_CoinChange.py
@@ -0,0 +1,27 @@
+from type_checking import *
+#https://leetcode.com/problems/coin-change/
+"""
+purpose: smallest number to make up to certain amount
+f(x) returns smallest number of coins to make up to amount "x"
+   f(x) => min( 1+f(x-C0), 1+f(x-C1), 1+f(x-C2), .....1+f(x-C3))
+"""
+from functools import cache
+import math
+def coinChange(coins: List[int], amount: int) -> int:
+    @cache
+    def minCoins(left):
+        if left == 0:
+            return 0
+        if left < 0:
+            return float("inf")
+
+        minimal = float("inf")
+        for i in coins:
+            if i <= left:
+                minimal= min(minCoins(left-i),minimal)
+        return minimal+1
+
+    re = minCoins(amount)
+
+    return -1 if math.isinf(re) else re
+
