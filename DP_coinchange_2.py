75bd42ae3f9a79a7f7179fdee2207241b570a950
Sisi Zhang <zhangsisi0717@gmail.com>
2021-06-08 17:20:30 -0500

    dp

---
+++ DP_coinchange_2.py
@@ -0,0 +1,27 @@
+##https://leetcode.com/problems/coin-change-2/
+"""
+sum(amount,k) returns the number of unique combinations if only considering first "K" elements, with sum to amount
+
+sum(amount,k) = summmation of (sum(amount - i*coins[k], k-1) , i from 0 to amount//coins[k]
+"""
+def change(amount, coins):
+    def sum(amount,k):
+        if k ==0:
+            return 1 if amount % coins[0] == 0 else 0
+
+        if amount <0:return 0
+        if amount ==0: return 1
+
+        total=0
+        for i in range(0,amount//coins[k]+1):
+            total += sum(amount-i*coins[k],k-1)
+
+        return total
+
+    return sum(amount,len(coins)-1)
+
+
+
+amount = 500
+coins=[1,2,5]
+change(amount, coins)
