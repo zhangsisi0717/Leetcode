b8108e8d576b1f595438b330db65d87da4b19c2e
Sisi Zhang <zhangsisi0717@gmail.com>
2021-06-17 13:20:24 -0500

    simplifyPath

--- Simplify_Path.py
+++ Simplify_Path.py
@@ -1,6 +1,27 @@
 "/home/./sisi/../../yuchen"
 #https://leetcode.com/problems/simplify-path/
-def simplifyPath(path):
-    dirs = path.split("/")
+from collections import deque
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        dirs = path.split("/")
+        stack = deque([])
+        for i in dirs:
+            if i in ("","."):
+                continue
+
+            elif i == "..":
+                if stack:
+                    stack.pop()
+
+            else:
+                stack.append(i)
+
+        re = "/" + "/".join(stack)
+        return re
+
+
+
+
+
 
 
