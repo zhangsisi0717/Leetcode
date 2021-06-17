343bda3665f0762b9548a36294d76ed5b733aafc
Sisi Zhang <zhangsisi0717@gmail.com>
2021-06-17 13:23:10 -0500

    simplifyPath

--- Stack_applications.py
+++ Stack_applications.py
@@ -1,2 +1,4 @@
 ##1.https://leetcode.com/problems/daily-temperatures/
 ##2. https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii
+##3. https://leetcode.com/problems/decode-string/
+##4. #https://leetcode.com/problems/simplify-path/g
