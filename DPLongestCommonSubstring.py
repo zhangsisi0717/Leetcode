c4fa3f6e97e5bbc8a76b450bf5be9f1fc1d9264b
Sisi Zhang <zhangsisi0717@gmail.com>
2021-06-19 15:06:38 -0500

    longestCommonSubstring

--- DPLongestCommonSubstring.py
+++ DPLongestCommonSubstring.py
@@ -1,3 +1,21 @@
+from typing import List
+##https://leetcode.com/problems/maximum-length-of-repeated-subarray/
+"""
+find longest common substring: set i,j as suffix of nums2 and nums1
+create a table to keep track of the longest common string ending at index i and j
+ if num1[j] == num2[i] => then lcs[i][j] == lcs[i-1][j-1] + 1 else 0
+"""
+def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+    lcs_suffix = [[0 for i in range(len(nums1)+1)] for j in range(len(nums2)+1)]
+    lcs = 0
+    for i in range(1,len(nums2)+1): ##i => nums2 , j => nums1
+        for j in range(1,len(nums1)+1):
+            lcs_suffix[i][j] = lcs_suffix[i-1][j-1]+1 if nums2[i-1] == nums1[j-1] else 0
+            if lcs_suffix[i][j] >lcs:
+                lcs = lcs_suffix[i][j]
+
+    return lcs
+
 def lcsubstring(stringA, stringB): ###https://www.techiedelight.com/longest-common-substring-problem/
     table =[[0 for i in range(len(stringA)+1)]for j in range(len(stringB)+1)]
     curLongest=0
