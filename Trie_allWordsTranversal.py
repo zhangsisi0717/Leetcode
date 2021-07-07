0574aac59acba9074db2609ab3127f61eb412fb8
Sisi Zhang <zhangsisi0717@gmail.com>
2021-07-07 17:51:32 -0500

    Trie and BinarySearch

--- Trie_allWordsTranversal.py
+++ Trie_allWordsTranversal.py
@@ -1,8 +1,9 @@
 from typing import List
 products = ["bags","baggage","banner","box","cloths"]
 """
-create a the trie first
+create a the trie first (only with "end")
 """
+products = ["bags","baggage","banner","box","cloths"]
 wordTrie = dict()
 products.sort()
 for w in products: ##create trie
@@ -13,6 +14,32 @@ for w in products: ##create trie
         cur_dic = cur_dic[w[idx]]
         if idx == len(w)-1:
             cur_dic["end"] = w
+
+###############################################
+"""
+or we can add all the words into the wordTrie, and then we do not neet to traverse the words at all, but would
+waster huge memroy!!! 
+"""
+"""
+if want to create all the words
+"""
+products = ["bags","baggage","banner","box","cloths"]
+wordTrie = dict()
+products.sort()
+for w in products: ##create trie
+    cur_dic = wordTrie
+    for idx in range(len(w)):
+        if w[idx] not in cur_dic:
+            cur_dic[w[idx]] = dict()
+            cur_dic[w[idx]]["allWords"] = []
+        cur_dic = cur_dic[w[idx]]
+        cur_dic["allWords"].append(w)
+        if idx == len(w)-1:
+            cur_dic["end"] = w
+
+searchWord = "bag"
+re = wordTrie["b"]["a"]["g"]["allWords"]
+
 
 """
 after create the Trie, we try to traverse all the words using DFS starting from current_dict
@@ -33,6 +60,8 @@ def wordTrans(cur):
     return result
 
 wordTrans(wordTrie)
+
+
 
 
 
