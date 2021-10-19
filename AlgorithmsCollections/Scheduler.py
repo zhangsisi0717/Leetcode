3304ab5a240db6d0d15c913721e65d5eb11f8f0c
Sisi Zhang <zhangsisi0717@gmail.com>
2021-10-18 21:42:53 -0700

    DP_maximumProfitJobScheduling

---
+++ AlgorithmsCollections/Scheduler.py
@@ -0,0 +1,17 @@
+import typing
+"""
+1. https://leetcode.com/problems/course-schedule-iii/
+courses = [[100,200],[200,1300],[1000,1250],[2000,3200]]
+x[0]:duration of the course 
+x[1]: end day of this course[x1]
+return: number of maximum course we can take 
+"""
+
+
+"""
+2.https://leetcode.com/problems/maximum-profit-in-job-scheduling/
+startTime = [1,2,3,3], endTime = [3,4,5,6], profit = [50,10,40,70]
+give startTime, endTime, and profit of each task
+return Maximum profit we can get (tasks should have overlap)
+
+"""
---
+++ DP_MaximumProfitJobScheduling.py
@@ -0,0 +1,33 @@
+from functools import cache
+"""
+step 1: sort the list by startTime
+step 2: maxProfit(i): max profit we can get starting considering from index i
+    maxProfit(i) = max( profit[i] + maxProfit(j) , maxProfit(i+1) )
+                       (j is the smallest index after index i that is not overlapped with task i)
+                       
+"""
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        tasks = [[startTime[i],endTime[i],profit[i]] for i in range(len(profit))]
+        tasks = sorted(tasks)
+
+        @cache
+        def maxProfit(index):
+            if index==len(profit)-1:
+                return tasks[index][2]
+
+            if index >=len(profit):
+                return 0
+
+            include, not_include = tasks[index][2],0
+            if tasks[index+1][0] < tasks[index][1]: #there is overlap
+                for i in range(index+2, len(profit)):
+                    if tasks[i][0] >= tasks[index][1]: # no overlap
+                        include += maxProfit(i)
+                        break
+            else:
+                include += maxProfit(index+1)
+            not_include = maxProfit(index+1)
+            return max(include, not_include)
+
+        return maxProfit(0)
