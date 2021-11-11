caac1798023a3c33a97fb6a4746c611e3f32cdd5
Sisi Zhang <zhangsisi0717@gmail.com>
2021-11-10 19:29:14 -0800

    rotate matrix

--- ValidateBinarySearchTree.py
+++ ValidateBinarySearchTree.py
@@ -6,6 +6,7 @@
 #         self.right = right
 
 import math
+from typing import *
 class Solution: #https://leetcode.com/problems/validate-binary-search-tree/
     def isValidBST(root: TreeNode) -> bool:
 
