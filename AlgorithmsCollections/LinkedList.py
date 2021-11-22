04b894a1cf4b74491b7999cd668225acaf9c9f05
Sisi Zhang <zhangsisi0717@gmail.com>
2021-11-21 21:18:08 -0800

    linkedList

--- AlgorithmsCollections/LinkedList.py
+++ AlgorithmsCollections/LinkedList.py
@@ -1,20 +1,3 @@
-class Node:
-    def __init__(self,value,restLinkedList):
-        self.value = value
-        self.next = restLinkedList
-
-
-class RecursiveLinkedList:
-    def __init__(self, head=None):
-        self.head = head
-
-
-    def add(self,value):
-        if not self.head:
-            self.head = Node(value,RecursiveLinkedList())
-            return self.head
-
-        else:
-            newNode = Node(value,None)
+##recu
 
 
